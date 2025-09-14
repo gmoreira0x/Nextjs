e34c29c75f3f40b8ec1dafdd5de14ccf5e2249b2
Gabriel <gabriel.scairp@gmail.com>
2025-09-14 00:06:22 -0300

    feat: Configura deploy para GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Gera uma pasta 'out' com arquivos estáticos
+  output: 'export',
+
+  // IMPORTANTE: Substitua 'appnext' pelo nome exato do seu repositório no GitHub
+  basePath: '/Nextjs',
+
+  // Desativa a otimização de imagens, que não funciona no modo estático
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
